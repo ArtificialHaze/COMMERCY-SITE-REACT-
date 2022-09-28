3ad3e97e7ecf22df054802868abe4bbbba4e81f3
ArtificialHaze <83704626+ArtificialHaze@users.noreply.github.com>
2022-09-28 19:21:20 +0400

    Add files via upload

---
+++ components.js
@@ -0,0 +1,6 @@
+export { default as Navbar } from "./Navbar";
+export { default as ProductList } from "./ProductList";
+export { default as Cart } from "./Cart";
+export { default as Details } from "./Details";
+export { default as Error } from "./Error";
+export { default as Modal } from "./Modal";
